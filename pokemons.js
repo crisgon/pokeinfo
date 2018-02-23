4a4e425ba7ed0d825998d3c4ceb3ea7d41835c37
malenacaroline <malenacaroline@hotmail.com>
2018-02-23 01:51:12 -0300

    Changing the layout of card

--- pokemons.js
+++ pokemons.js
@@ -36,8 +36,7 @@ function createCard () {
     </div>
     <div class="pokemon-info">
         <h1 class="name">${pokemon.name}</h1>
-        <h2 class="number">Nº ${pokemon.id}</h2>
-        <h3 class="type">Type: ${pokemon.types.map(item => item.type.name).toString()}</h3>
+        <font class="number">Nº ${pokemon.id} - Type: ${pokemon.types.map(item => item.type.name).toString()}</font>
         <h3 class="skill">Skills: ${pokemon.moves.map(item => ' ' + item.move.name).toString()}</h3>
         <h3 class="weight">Weight: ${pokemon.weight  / 10}kg</h3>
         <h3 class="height">Height: ${pokemon.height  / 10}m</h3>
